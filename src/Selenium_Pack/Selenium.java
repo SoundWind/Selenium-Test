f86e217c420d06dfcb8e32d298766324bd424675
Regino Cornejo <junior.destiny26@gmail.com>
2022-10-24 00:26:56 -0500

    Primer commit - Pruebas en Selenium segunda parte

---
+++ src/Selenium_Pack/Selenium.java
@@ -0,0 +1,16 @@
+package Selenium_Pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "C://Selenium/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		// inserta tu código aquí
+		Thread.sleep(5000);
+		driver.quit();
+	}
+}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author Regino
- *
- */
-module Pruebas {
-}
